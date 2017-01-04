6d45a06843d7d052f5c22657cc9664d16614a49b
Lex Myers <lex.myers@gmail.com>
2017-01-04 12:06:29 -0800

    TODOs are DONE

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,28 +1,39 @@
 var articles = [];
 
 function Article (options) {
-/* TODO: This is our Model constructor. It will take in
+/* DONE: This is our Model constructor. It will take in
    our source data from blogArticles and instantiate a
    new Object according to this new definition. options is
    a placeholder for the object that will ultimately be
    passed in. Use all of the properties in blogArticles
    to populate the new Article data that we'll use.  */
-   this.title = options.title;
+  this.title = options.title;
+  this.category = options.category;
+  this.author = options.author;
+  this.authorUrl = options.authorUrl;
+  this.publishedOn = options.publishedOn;
+  this.body = options.body;
 };
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-  /* TODO: We also need to fill in:
+  $newArticle.find('address:first-child').text(this.author);
+  $newArticle.find('address:first-child').attr('href', this.authorUrl);
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('section').html(this.body);
+  $newArticle.find('time').attr('datetime', this.publishedOn);
+  /* DONE: We also need to fill in:
   1. author name
   2. author url
   3. article title
   4. article body
-  5. publication*/
+  5. publication */
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
   $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-/* TODO: This cloned article is no longer a template, as it now
+/* DONE: This cloned article is no longer a template, as it now
 has real data attached to it. Remove the class from this new article! */
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 
